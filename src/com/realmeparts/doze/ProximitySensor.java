2523d800f6b1ef4e74f4d8dd4c94d3d271e7bd1e
4j17h <7hou74m4j17h@gmail.com>
2020-12-14 01:26:32 +0530

    RealmeParts: Doze: Fix null proximity sensor
    Signed-off-by: 4j17h <7hou74m4j17h@gmail.com>
    Change-Id: Ib6fedcf02fd1ebe32700cfbbbedaa3842caaa634

--- src/com/realmeparts/doze/ProximitySensor.java
+++ src/com/realmeparts/doze/ProximitySensor.java
@@ -50,7 +50,7 @@ public class ProximitySensor implements SensorEventListener {
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mSensor = DozeUtils.getSensor(mSensorManager, "android.sensor.proximity");
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
